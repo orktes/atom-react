43ebac43731fd7e4c732589b7f206261518290af
Jaakko Lukkari <jaakko@applifier.com>
2015-01-21 19:52:47 +0200

    Indent spec

---
+++ spec/fixtures/sample.jsx
@@ -0,0 +1,22 @@
+<div>
+<div>
+<div
+attr1=""
+attr2=""
+attr3="">
+</div>
+<div attr1="1"
+attr2="2"
+attr3="3">
+</div>
+<div
+attr1="1"
+attr2="2"
+attr3 />
+<div />
+</div>
+
+<div><div>
+<span />
+</div></div>
+</div>

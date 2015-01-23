34f596b7bf8a206b82ef28c5d424ff10af7210b5
Jaakko Lukkari <jaakko@applifier.com>
2015-01-23 15:16:54 +0200

    Generate tokens

--- Makefile
+++ Makefile
@@ -2,3 +2,13 @@ snippets:
 	curl -O "https://raw.githubusercontent.com/orktes/atom-react/master/snippets/JavaScript%20(JSX).cson"
 	cson2json JavaScript%20\(JSX\).cson > _data/snippets.json
 	rm JavaScript%20\(JSX\).cson
+	curl -O "https://raw.githubusercontent.com/orktes/atom-react/master/grammars/JavaScript%20(JSX).cson"
+	curl -O "https://raw.githubusercontent.com/atom/language-javascript/master/grammars/javascript.cson"
+	cson2json javascript.cson > javascript.json
+	cson2json JavaScript%20\(JSX\).cson > jsx.json
+	rm JavaScript%20\(JSX\).cson
+	rm javascript.cson
+	node generate_tokens.js
+	rm javascript.json
+	rm jsx.json
+	curl -o assets/css/atom-dark-syntax.css http://atom.github.io/highlights/examples/atom-dark.css
---
+++ generate_tokens.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var _ = require('lodash');
+
+var HighLights = require('highlights');
+
+var highlighter = new HighLights();
+
+var snippetsBase = require('./_data/snippets.json');
+var snippets = snippetsBase['.source.js'];
+
+function zeroFill( number, width )
+{
+  width -= number.toString().length;
+  if ( width > 0 )
+  {
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+  return number + ""; // always return a string
+}
+
+highlighter.registry.loadGrammarSync('./javascript.json');
+highlighter.registry.loadGrammarSync('./jsx.json');
+
+var i = 0;
+for (var key in snippets) {
+  var snippet = snippets[key];
+  snippet.html = highlighter.highlightSync({
+    fileContents: snippet.body,
+    scopeName: 'source.js.jsx'
+  });
+}
+
+fs.writeFileSync('./_data/snippets.json', JSON.stringify(snippetsBase, null, 2));
